9e5390c2b94e07d75e96884f5057a01f227b4c2b
im-noob <aaravonly4you@gmail.com>
2018-08-03 01:24:48 +0530

    fistst push

---
+++ Components/components1.js
@@ -0,0 +1,16 @@
+import React ,{Component} from 'react';
+import { Text,View  } from 'react-native';
+
+
+export default class Component1 extends Component{
+    render(){
+        return(
+            <View>
+                <Text> _____________________</Text>
+                <Text> _____________________</Text>
+                <Text> _____________________</Text>
+            </View>
+        );
+    }
+}
+
