59e788452ab80480037f3ff0d913f34613e4358a
im-noob <aaravonly4you@gmail.com>
2018-08-03 14:29:31 +0530

    kj

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React ,{Component} from 'react';
 import { Text,View , StyleSheet, TouchableHighlight,TouchableOpacity,TextInput,Switch } from 'react-native';
-import Component1 from './Components/amritesh/components1';
-import Component1 from './Components/amritesh/components2';
+import LoginScreen from './Components/login/LoginScreen';
+
 
 
 
@@ -9,9 +9,7 @@ export default class myapp extends Component{
   render(){
     return(
       <View>
-        <Text> _____________________</Text>
-        <Text> _____________________</Text>
-        <Text> _____________________</Text>
+        <LoginScreen/>
     
       </View>
     );    
