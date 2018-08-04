bdd2733390b9405bcbe1245fc9a13f67336a36bb
im-noob <aaravonly4you@gmail.com>
2018-08-05 01:06:51 +0530

    t

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React ,{Component} from 'react';
 import { Text,View , StyleSheet, TouchableHighlight,TouchableOpacity,TextInput,Switch } from 'react-native';
 
-<<<<<<< HEAD
-=======
 import MenuTest from "./Components/menu/Menu.js";
 
->>>>>>> 72ee9f2bce61d78c954452c3908e45af8943a92a
 
 
 import Login from './Components/login/Login';
@@ -31,14 +28,5 @@ export default class myapp extends Component{
         
       
        
-<<<<<<< HEAD
-
   }
 }
-=======
-  }
-}
-
-
-
->>>>>>> 72ee9f2bce61d78c954452c3908e45af8943a92a
